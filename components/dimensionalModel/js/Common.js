718effd265cb8d0a60281fd756f35b06efd750d5
yuki <yuki.itoh@front-artgraph.com>
2020-05-30 15:17:09 +0900

    👨‍💻Update!

--- components/dimensionalModel/js/Common.js
+++ components/dimensionalModel/js/Common.js
@@ -3,12 +3,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'// eslint-d
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'// eslint-disable-line
 import { TimelineMax, TweenMax } from 'gsap' // eslint-disable-line
 import EventBus from "~/utils/event-bus"; // eslint-disable-line
+// import dat from 'dat.gui';// eslint-disable-line
 
 class Common {
   constructor() {
     this.scene = null
     this.camera = null
-    this.light = null
+    this.spotLight = null
+    this.AmbientLight = null
     this.renderer = null
     this.size = {
       windowW: null,
@@ -28,19 +30,18 @@ class Common {
     this.tl = new TimelineMax()
     // renderer
     this.renderer = new THREE.WebGLRenderer({
-      canvas: $canvas
+      canvas: $canvas,
+      antialias: true
     })
     this.renderer.setClearColor(new THREE.Color(0x000000))
     this.renderer.shadowMap.enabled = true
     this.renderer.gammaOutput = true
     // scene
     this.scene = new THREE.Scene()
-    this.scene.fog = new THREE.Fog(0x000000, -20, 300)
-    // light
-    this.Light = new THREE.DirectionalLight(0xffffff, 4)
-    this.Light.position.set(40, 120, 80)
-    this.Light.castShadow = true
-    this.scene.add(this.Light)
+    this.scene.fog = new THREE.Fog(0x000000, -10, 300)
+
+    this.spotLightAdd()
+    this.ambientLightAdd()
     // camera
     this.camera = new THREE.PerspectiveCamera(
       45,
@@ -48,13 +49,16 @@ class Common {
       0.1,
       1000
     )
-    this.camera.position.set(0, 30, 150)
+    this.camera.position.set(0, 10, 100)
     this.camera.lookAt(this.scene.position)
     this.renderer.setSize(this.size.windowW, this.size.windowH)
     // this.cube()
-    this.Helpers()
+    // this.Helpers()
+    this.groundAdd()
+    // this.cube()
     this.gltfModel()
-    this.dustAdd()
+    // this.dustAdd()
+    this.datGUI()
   }
 
   setSize() {
@@ -80,16 +84,43 @@ class Common {
     console.log(direction)
   }
 
+  spotLightAdd() {
+    this.spotLight = new THREE.SpotLight(0xffffff)
+    this.spotLight.position.set(100, 60, 30)
+    this.spotLight.angle = Math.PI / 8
+    console.log(this.spotLight.angle)
+    this.spotLight.penumbra = 0.5
+    this.spotLight.decay = 2
+    this.spotLight.distance = 300
+    this.spotLight.castShadow = true
+    this.spotLight.shadow.mapSize.width = 1024
+    this.spotLight.shadow.mapSize.height = 1024
+    this.spotLight.shadow.camera.near = 10
+    this.spotLight.shadow.camera.far = 200
+    this.scene.add(this.spotLight)
+  }
+
+  ambientLightAdd() {
+    this.AmbientLight = new THREE.AmbientLight(0xe4e2c9, 0.2)
+    this.scene.add(this.AmbientLight)
+  }
+
   gltfModel() {
     this.dracoLoader = new DRACOLoader()
     this.dracoLoader.setDecoderPath('/draco/')
     this.loader = new GLTFLoader()
     this.loader.setDRACOLoader(this.dracoLoader)
-    const url = '/gltf/tel_min.gltf'
+    const url = '/gltf/ayumu/body.gltf'
     this.loader.load(url, (gltf) => {
       this.model = gltf.scene
-      this.model.scale.set(10.0, 10.0, 10.0)
+      this.model.scale.set(20, 20, 20)
       this.model.castShadow = true
+      gltf.scene.traverse(function(node) {
+        if (node.isMesh) {
+          node.castShadow = true
+        }
+      })
+      this.model.position.y = -20
       this.scene.add(this.model)
     })
   }
@@ -124,7 +155,7 @@ class Common {
     // const material = new THREE.MeshBasicMaterial({ map: texture })
     let material = new THREE.PointsMaterial()
     if (material.map) {
-      material.map.dispose() // これを必ず呼ぶこと！！！
+      material.map.dispose()
       material.map = texture
     } else {
       material = new THREE.PointsMaterial({
@@ -136,6 +167,7 @@ class Common {
     }
 
     this.dusts = new THREE.Points(geometry, material)
+
     this.tl.to(this.dusts.rotation, 600, {
       y: -2 * Math.PI,
       repeat: -1
@@ -143,13 +175,72 @@ class Common {
     this.scene.add(this.dusts)
   }
 
+  cube() {
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x4080ff,
+      dithering: true
+    })
+    const geometry = new THREE.BoxBufferGeometry(10, 60, 10)
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.castShadow = true
+    mesh.rotation.y = 0.5 * Math.PI
+    this.scene.add(mesh)
+  }
+
+  groundAdd() {
+    // const groundGeometry = new THREE.BoxGeometry(1000000, 0, 1000000)
+    // const groundMaterial = new THREE.MeshBasicMaterial({
+    //   color: 0xffffff
+    // })
+    // const ground = new THREE.Mesh(groundGeometry, groundMaterial)
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xf1f1f1,
+      dithering: true
+    })
+    const geometry = new THREE.PlaneBufferGeometry(1000000, 1000000)
+    const ground = new THREE.Mesh(geometry, material)
+    ground.position.y = -22
+    ground.rotation.x = -Math.PI * 0.5
+    ground.receiveShadow = true
+    this.scene.add(ground)
+  }
+
   Helpers() {
     const axes = new THREE.AxesHelper(250)
     this.scene.add(axes)
     const gridHelper = new THREE.GridHelper(200, 50) // 引数は サイズ、1つのグリッドの大きさ
     this.scene.add(gridHelper)
-    const LightHelper = new THREE.DirectionalLightHelper(this.Light)
+    const LightHelper = new THREE.SpotLightHelper(this.spotLight)
     this.scene.add(LightHelper)
+
+    const spotLightShadowHelper = new THREE.CameraHelper(
+      this.spotLight.shadow.camera
+    )
+    this.scene.add(spotLightShadowHelper)
+  }
+
+  datGUI() {
+    const dat = require('dat.gui') // eslint-disable-line
+    this.gui = new dat.GUI()
+    console.log(this.gui)
+    console.log(this.spotLight)
+
+    const light = this.gui.addFolder('Light')
+    light.add(this.spotLight, 'penumbra', 1, 2).listen()
+    light.add(this.spotLight, 'decay', 0, 10).listen()
+    light.add(this.spotLight, 'distance', 1, 1000).listen()
+    light.add(this.spotLight, 'angle', 0, Math.PI * 2).listen()
+    light.open()
+
+    const lightPos = this.gui.addFolder('Light Position')
+    lightPos.add(this.spotLight.position, 'x', -200, 200).listen()
+    lightPos.add(this.spotLight.position, 'y', 0, 200).listen()
+    lightPos.add(this.spotLight.position, 'z', -200, 200).listen()
+    lightPos.open()
+
+    const camera = this.gui.addFolder('Camera')
+    camera.add(this.camera.position, 'z', 50, 200).listen()
+    camera.open()
   }
 }
 export default new Common()
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
   .container
+    .first-maril
+      .first-maril__wrap
+        h1 info@zypressen.org
+      .second__text
+        p We are a creative team with a small group of people. Please feel free to contact us anytime. Your Friendly Neighbor  ZYPRESSEN.
 </template>
 
 <script>
