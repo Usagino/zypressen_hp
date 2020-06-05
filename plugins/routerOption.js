bda1326ae0446400842bc8243fdda91ed8a8f173
yuki <yuki.itoh@front-artgraph.com>
2020-06-05 17:58:01 +0900

    👨‍💻Update! oneliner

--- plugins/routerOption.js
+++ plugins/routerOption.js
@@ -2,17 +2,6 @@ import EventBus from "~/utils/event-bus"; // eslint-disable-line
 
 export default async ({ app }) => {
   await app.router.afterEach((to, from) => {
-    // ページ遷移ごとに行いたい処理
-    const htmlElement = document.querySelector('html')
-    const bodyElement = document.querySelector('body')
-
-    if (to.path === '/' || to.path === '/contact' || to.path === '/works') {
-      htmlElement.classList.add('noscroll')
-      bodyElement.classList.add('noscroll')
-    } else {
-      htmlElement.classList.remove('noscroll')
-      bodyElement.classList.remove('noscroll')
-    }
     console.log(to.path, from.path)
 
     switch (to.path) {
