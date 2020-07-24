8f4f745edef6ca7e0deed315c8258a55fded5577
yuki <yuki.itoh@front-artgraph.com>
2020-07-24 16:13:44 +0900

    added loading

--- components/PageLoading.vue
+++ components/PageLoading.vue
@@ -1,20 +1,38 @@
-<template>
-  <div v-show="loading" class="loading">
-    <div class="loading__loader">Loading...</div>
-  </div>
+<template lang="pug">
+  transition(name="fade")
+    .loading(v-show="loading && animateProgress")
+      img.loading__logo(src="/PageLogo.svg")
+      .loading__progress
+        .loading__progress__bar
 </template>
 
 <script>
+import { gsap } from 'gsap' // eslint-disable-line
+
 export default {
   name: 'Loading',
   data() {
     return {
-      loading: false
+      loading: true,
+      animateProgress: true
     }
   },
   mounted() {
-    const target = document.querySelector('.webgl')
-    console.log(target)
+    // const target = document.querySelector('.webgl')
+    // console.log(target)
+    const tl = gsap.timeline()
+    tl.set('html,body', { overflow: 'hidden' })
+      .to('.loading__progress__bar', 9, {
+        scaleX: '1',
+        delay: 1
+      })
+      .set('html,body', {
+        overflow: 'scroll',
+        onComplete: () => {
+          console.log(this.animateProgress)
+          this.animateProgress = false
+        }
+      })
   },
   methods: {
     start() {
@@ -35,16 +53,45 @@ export default {
   bottom: 0;
   right: 0;
   width: 100vw;
-  height: 100vh;
+  height: var(--wh, 100vh);
   z-index: 9999;
   transition: all 1.2s ease;
   background: $color-black;
-  &__loader {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 18px;
+  display: grid;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  place-items: center;
+  grid-gap: $pri-value;
+  padding: $pri-value * 2;
+  box-sizing: border-box;
+  &__logo {
+    height: 40px;
+    width: auto;
   }
+  &__progress {
+    width: 300px;
+    height: 8px;
+    content: '';
+    display: block;
+    background: $color-gray;
+    &__bar {
+      transform-origin: left center;
+      width: 100%;
+      height: 100%;
+      content: '';
+      display: block;
+      background: $color-orange;
+      transform: scaleX(0);
+    }
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+  transition-delay: 1s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -46,7 +46,6 @@ export default {
       linkStyle
     )
   },
-
   methods: {}
 }
 </script>
