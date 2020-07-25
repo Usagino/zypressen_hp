77cde0377fe8baa1cf98b1d8234a4e2b2660b845
yuki <yuki.itoh@front-artgraph.com>
2020-07-25 17:05:04 +0900

    remove common file

--- plugins/routerOption.js
+++ plugins/routerOption.js
@@ -1,7 +1,6 @@
 import Vue from 'vue' // eslint-disable-line
 import EventBus from "~/utils/event-bus"; // eslint-disable-line
 import { gsap } from 'gsap' // eslint-disable-line
-import Common from '@/components/DimensionalModel/js/Common.js' // eslint-disable-line
 
 export default async ({ app, params }) => {
   await app.router.afterEach((to, from) => {
