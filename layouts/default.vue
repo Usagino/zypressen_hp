4ed4c3abfa3802edfa187b03a47ffd9beecd9cd8
yuki <yuki.itoh@front-artgraph.com>
2020-07-26 16:30:45 +0900

    I removed something unnecessary to make it new.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,5 @@
 <template lang="pug">
   .page
-    nuxt-link.page__logo(to="/" no-prefetch)
-      img.page__logo__image(src="/PageLogo.svg")
-    PageLoading
-    PageMenu
-    DimensionalModel
     nuxt
 </template>
 
@@ -53,61 +48,5 @@ export default {
 <style lang="scss">
 html {
   background: $color-black;
-}
-.page {
-  min-height: var(--wh, 100vh);
-  width: 100vw;
-  //overflow: hidden;
-  &__logo {
-    mix-blend-mode: difference;
-    position: fixed;
-    top: $pri-value;
-    left: $pri-value;
-    height: 16px;
-    font-size: medium;
-    z-index: 1000;
-    @include mq(sm) {
-      left: 0px;
-      right: 0px;
-      margin: auto;
-      width: fit-content;
-      top: 32px;
-    }
-    img {
-      height: 16px;
-      width: auto;
-    }
-  }
-  &__copylight {
-    position: fixed;
-    bottom: $pri-value;
-    right: $pri-value;
-    z-index: 1000;
-    display: flex;
-    @include gap-right(16px);
-    @include mq(sm) {
-      display: none;
-    }
-    &__text {
-      @include font-nav-secondary;
-      line-height: 18px;
-    }
-    &__image {
-      font-size: medium;
-      height: 16px;
-      width: 16px;
-      img {
-        height: 100%;
-        width: 100%;
-      }
-    }
-  }
-}
-.testbuton {
-  cursor: pointer;
-  position: fixed;
-  top: 20px;
-  left: 20px;
-  z-index: 1000;
 }
 </style>
